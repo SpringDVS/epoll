e0e5faf9d365dd0716d44d0232b528dfd917863e
Nathan Sizemore <nathanrsizemore@gmail.com>
2015-10-04 12:47:54 -0400

    0.2.1 docs

---
+++ implementors/std/error/trait.Error.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['epoll'] = ["impl <a class='trait' href='http://doc.rust-lang.org/nightly/std/error/trait.Error.html' title='std::error::Error'>Error</a> for <a class='enum' href='epoll/util/enum.CreateError.html' title='epoll::util::CreateError'>CreateError</a>","impl <a class='trait' href='http://doc.rust-lang.org/nightly/std/error/trait.Error.html' title='std::error::Error'>Error</a> for <a class='enum' href='epoll/util/enum.CtlError.html' title='epoll::util::CtlError'>CtlError</a>","impl <a class='trait' href='http://doc.rust-lang.org/nightly/std/error/trait.Error.html' title='std::error::Error'>Error</a> for <a class='enum' href='epoll/util/enum.WaitError.html' title='epoll::util::WaitError'>WaitError</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
